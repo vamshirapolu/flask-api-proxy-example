d4a4631923e32d4ad481b4792d0dd1fb4582c093
Vamshi Rapolu <vamshi.rapolu@gmail.com>
2019-06-10 11:12:26 -0400

    Removed the methods param

--- lambda_function.py
+++ lambda_function.py
@@ -14,17 +14,17 @@ api = Api(blueprint, doc='/api-doc/')
 
 app.register_blueprint(blueprint)
 
-@api.route('/', methods=['GET'])
+@api.route('/')
 def index():
     return jsonify(status=200, message='OK')
 
 
-@api.route('/greet/<path:user>', endpoint='greet', methods=['GET'])
+@api.route('/greet/<path:user>', endpoint='greet')
 def greetings(user):    
     return jsonify(status=200, message='Hello {}!'.format(user))
 
 
-@api.route('/greetuser', endpoint='greet_user', methods=['GET'])
+@api.route('/greetuser', endpoint='greet_user')
 def greetingsByRequestParam():
     print(request.args)
     user = request.args.get('user')
