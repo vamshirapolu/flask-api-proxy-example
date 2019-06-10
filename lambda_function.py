c531e5e917b3674ced833c55ff32c2aed34b8ed4
Vamshi Rapolu <vamshi.rapolu@gmail.com>
2019-06-10 13:03:06 -0400

    flask swagger updated

--- lambda_function.py
+++ lambda_function.py
@@ -4,34 +4,9 @@ from flask import (
     jsonify, 
     request
 )
-from flask_swagger_ui import get_swaggerui_blueprint
+from flask_swagger import swagger
 
 app = Flask(__name__)
-
-SWAGGER_URL = '/api/docs'
-API_URL = 'swagger.json'
-
-# Call factory function to create our blueprint
-swaggerui_blueprint = get_swaggerui_blueprint(
-    # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
-    SWAGGER_URL,
-    API_URL,
-    config={  # Swagger UI config overrides
-        'app_name': "Test application"
-    },
-    # oauth_config={ # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
-    # 'clientId': "your-client-id",
-    # 'clientSecret': "your-client-secret-if-required",
-    # 'realm': "your-realms",
-    # 'appName': "your-app-name",
-    # 'scopeSeparator': " ",
-    # 'additionalQueryStringParams': {'test': "hello"}
-    # }
-)
-
-# Register blueprint at URL
-# (URL must match the one given to factory function above)
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 app.run()
 
@@ -50,6 +25,14 @@ def greetingsByRequestParam():
     print(request.args)
     user = request.args.get('user')
     return jsonify(status=200, message='Hello {}!'.format(user))
+
+
+@app.route("/api-doc")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
 
 def lambda_handler(event, context):
     print(event)
