d5c4053b02a2590229e32946859ca815ec0de16e
Vamshi Rapolu <vamshi.rapolu@gmail.com>
2019-06-09 20:07:44 -0400

    Request user param

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,26 @@
 import awsgi
 from flask import (
     Flask,
-    jsonify,
+    jsonify, 
+    request
 )
 
 app = Flask(__name__)
+app.run(debug=True)
 
-@app.route('/')
+@app.route('/', method='GET')
 def index():
     return jsonify(status=200, message='OK')
 
 
-@app.route('/greet/<path:user>')
+@app.route('/greet/<path:user>', method='GET')
 def greetings(user):    
+    return jsonify(status=200, message='Hello {}!'.format(user))
+
+@app.route('/greet', method='GET')
+def greetingsByRequestParam():
+    print(request.args)
+    user = request.args.get('user')
     return jsonify(status=200, message='Hello {}!'.format(user))
 
 def lambda_handler(event, context):
