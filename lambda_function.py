c5c3e9db341dfec1052774700130f78ce3d9bd92
Vamshi Rapolu <vamshi.rapolu@gmail.com>
2019-06-10 15:46:57 -0400

    base_path updated

--- lambda_function.py
+++ lambda_function.py
@@ -16,7 +16,8 @@ fields = {
     })
 }
 
-base_path = os.path.dirname(os.path.abspath(__file__))
+print(__file__)
+base_path = os.path.dirname(os.path.realpath(__file__))
 print('base_path {}'.format(base_path))
 
 @app.route('/', methods=['GET'])
